e7bda5e0585693b172b89af5d1b0d1aeb7868d5a
danielniebles <danielf.niebles@gmail.com>
2025-01-16 10:52:47 -0500

    feat: add shadcn for components and extend tailwind config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl">Home</h1>
-    </div>
+    <>
+      <section className="pink_container">
+
+        <h1 className="heading">Pitch your startup, <br /> Connect with entrepenurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      </section>
+    </>
   );
 }
