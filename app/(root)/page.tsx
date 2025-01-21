318fd7d6a69e2e65ca559bf203ed79d5904889ea
danielniebles <danielf.niebles@gmail.com>
2025-01-21 18:04:25 -0500

    feat: add sanity generated types for schemas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,11 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query: string }> }) {
   const query = (await searchParams).query
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: { _id: 1, name: 'Daniel Niebles' },
-    _id: 1,
-    description: 'This is the description',
-    image: 'https://images.unsplash.com/photo-1721332149274-586f2604884d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    category: 'Robots',
-    title: 'We Robots'
-  }]
+  const posts = await client.fetch(STARTUPS_QUERY)
 
   return (
     <>
@@ -26,7 +19,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         <p className="text-30-semibold">{query ? `Search results for: ${query}` : 'All Startups'}</p>
         <ul className="mt-7 card_grid">
           {
-            posts?.length > 0 ? posts.map((post) => (
+            posts?.length > 0 ? posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post}/>
             )) : <p>No startups found</p>
 
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,9 +3,12 @@ import { EyeIcon } from 'lucide-react'
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
 
-export default function StartupCard({ post }: { post: PostType }) {
-  const { _id, _createdAt, author: { _id: _authorId, name }, views, title, category, image, description } = post
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author }
+
+export default function StartupCard({ post }: { post: StartupTypeCard }) {
+  const { _id, _createdAt, author, views, title, category, image, description } = post
 
   return (
     <li className="startup-card group">
@@ -18,14 +21,14 @@ export default function StartupCard({ post }: { post: PostType }) {
       </div>
       <div className="flex-between gap-5 mt-5">
         <div className="flex-1">
-          <Link href={`/user/${_authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?.id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${_authorId}`}>
+        <Link href={`/user/${author?.id}`}>
           <Image src='https://placehold.co/48x48' alt='placeholder' className='rounded-full' height={48} width={48} />
         </Link>
       </div>
