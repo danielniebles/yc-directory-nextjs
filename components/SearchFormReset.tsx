7422212cdd1a0d954596925b16f656f62b5bbbe9
danielniebles <danielf.niebles@gmail.com>
2025-01-21 18:43:58 -0500

    feat: add search parameters to query

--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -6,7 +6,6 @@ export default function SearchFormReset() {
   const reset = () => {
     const form = document.querySelector('.search-form') as HTMLFormElement
     if (form) form.reset()
-
   }
   return (
     <button type="reset" onClick={reset}>
