de4648a7faf954e0bc53d2c02ea7e9eac2749189
danielniebles <danielf.niebles@gmail.com>
2025-01-29 16:00:52 -0500

    task: fix navbar buttons in mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 export default async function Navbar() {
   const session = await auth()
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -15,16 +18,22 @@ export default async function Navbar() {
           {session && session?.user ?
             <>
               <Link href='/startup/create'>
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
               <form action={async () => {
                 "use server"
                 await signOut({ redirectTo: "/" })
               }}>
-                <button type="submit">Logout</button>
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500 mt-1" />
+                </button>
               </form>
-              <Link href={`/user/${session?.user?.id}`}>
-                {session?.user?.name}
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-10">
+                  <AvatarImage src={session?.user?.image || ''} alt={session?.user?.name || ''} />
+                </Avatar>
               </Link>
             </>
             : <form action={async () => {
